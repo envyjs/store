337750d00fdf3d700ba8e516669c46b23dc3faac
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2025-02-27 02:41:51 +0000

    Update app.js

--- guide/envy/linux/app.js
+++ guide/envy/linux/app.js
@@ -1,11 +1,9 @@
 (function() {
   const windowTitle = 'Linux';
   const customHTML = `
-  <iframe id="emb" src="http://store.envy.ink/guide/envy/linux/index.htmlcpu=x86">
+  <iframe id="emb" src="http://store.envy.ink/guide/envy/linux/index.html?cpu=x86">
   `;
 
   // Create a window dynamically
   createWindow(windowTitle, customHTML, 720, 580);
 })();
-
-notifier.create('Hello from the Example App', 'info', 5000);
